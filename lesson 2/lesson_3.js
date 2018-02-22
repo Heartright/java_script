ad5cf08ce3695c14cdf8f3c489dbf6cc102e691a
heartright <rodex.lani@gmail.com>
2018-02-22 16:35:30 +0300

    fix_22_02

---
+++ lesson 2/lesson_3.js
@@ -0,0 +1,20 @@
+1
+var arr = ['a', 'b', 'c', 'd']
+			alert(arr);
+
+
+
+2
+var arr = ['a', 'b', 'c', 'd']
+			alert(arr[0]);
+			alert(arr[1]);
+			alert(arr[2]);
+
+3
+var arr = ['a', 'b', 'c', 'd'];
+alert(arr[0] + '+' + arr[1] + ', ' + arr[2] + '+' + arr[3]);		
+
+4
+			var arr = [2, 5, 3, 9]
+			var result = arr[0] * arr[1] + arr[2] * arr[3]; 
+			alert(result);
