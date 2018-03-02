43ff52c53f4e8b5154820eecbe722ee79ed45466
heartright <rodex.lani@gmail.com>
2018-03-02 15:23:19 +0300

    fix_02_03

---
+++ lesson 3/lesson_3.js
@@ -0,0 +1,141 @@
+1
+			var a = -3;
+			if (a == 0) {
+				alert('Верно');
+			}
+			else{
+				alert('Неверно')
+			}
+
+2
+	var a = -3;
+			if (a > 0) {
+				alert('Верно');
+			}
+			else{
+				alert('Неверно')
+			}
+
+3
+var a = 0;
+			if (a < 0) {
+				alert('Верно');
+			}
+			else{
+				alert('Неверно')
+			}		
+
+4
+	var a = 1;
+			if (a >= 0) {
+				alert('Верно');
+			}
+			else{
+				alert('Неверно')
+			}
+5
+	var a = 1;
+			if (a <= 0) {
+				alert('Верно');
+			}
+			else{
+				alert('Неверно')
+			}
+	
+6
+	var a = 0;
+			if (a != 0) {
+				alert('Верно');
+			}
+			else{
+				alert('Неверно')
+			}	
+7
+
+var a = 'test';
+			if (a == 'test') {
+				alert('Верно');
+			}
+			else{
+				alert('Неверно')
+			}
+
+8
+var a = '1';
+			if (a == '1') {
+				alert('Верно');
+			}
+			else{
+				alert('Неверно')
+			}
+9
+var obj = [ [1, 2, 3], [4, 5, 6], [7,8,9] ];
+			alert(obj[1][0]);
+	
+	
+10
+var test = false;
+			if (test != true) {
+				alert('Верно');
+			}
+			else{
+				alert('Неверно')
+			}
+
+11
+var a = 2;
+			if (a > 0 && a < 5) {
+				alert('Верно');
+			}
+			else{
+				alert('Неверно')
+			}
+			
+12
+var a = 2;
+			if (a == 0 || a == 2) {
+				a += 7;
+			}
+			else{
+				a /= 10;
+			}
+			alert(a);
+13 
+var a = 3;
+			var b = 5;
+			if (a <= 1 && b >= 3) {
+				alert(a + b);
+			}
+			else{
+				alert(a - b);
+			}
+14
+var a = 3;
+			var b = 5;
+			if ((a > 2 && a < 11) || (b >= 6 && b < 14)) {
+				alert('Верно');
+			}
+			else{
+				alert('Неверно');
+			}		
+15
+	var num = 2;.
+		switch (num){
+			case 1:
+		result = 'зима';
+		break;
+		switch (num)
+			case 2:
+		result = 'Весна';
+		break;
+		switch (num)
+			case 1:
+		result = 'Лето';
+		break;
+		switch (num)
+			case 1:
+		result = 'Осень';
+		break;
+		}
+		alert(num);
+	
