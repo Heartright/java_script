d730b3fcaba54ef77bc302a243aa3f37591b2668
heartright <rodex.lani@gmail.com>
2018-03-14 18:51:48 +0300

    fix_14_03

--- lesson 4/lesson_4.js
+++ lesson 4/lesson_4.js
@@ -109,106 +109,36 @@ var array = ['10', '20', '30', '50', '235', '3000'];
 				}
 			}
 13 
-var a = 3;
-			var b = 5;
-			if (a <= 1 && b >= 3) {
-				alert(a + b);
+var array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+		var str = '';
+			for (var i = 0; i < array.length; i++){
+				str += '-' + array[i];
 			}
-			else{
-				alert(a - b);
-			}
+		document.write(str + '-'); 
 14
-var a = 3;
-			var b = 5;
-			if ((a > 2 && a < 11) || (b >= 6 && b < 14)) {
-				alert('Верно');
+var array = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
+			for (var i = 0; i < array.length; i++){
+				if ( i == 5 || i == 6 ){
+					document.write('<b>' + array[i] + '</b><br>');
+				}
+				else{
+					document.write(array[i]+ '<br>');
+				}
 			}
-			else{
-				alert('Неверно');
-			}		
+		document.write(str + '-'); 	
 15
-	var num = 3;
-		switch (num){
-			case 1:
-		result = 'зима';
-		break;
-			case 2:
-		result = 'Весна';
-		break;
-			case 3:
-		result = 'Лето';
-		break;
-			case 4:
-		result = 'Осень';
-		break;
-		}
-		alert(result);
+var array = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
+		var day = 2;
+			for (var i = 0; i < array.length; i++){
+				if ( i == day ){
+					document.write('<i>' + array[i] + '</i><br>');
+				}
+				else{
+					document.write(array[i]+ '<br>');
+				}
+			}
 	
 16
-var day = 21;
-		if 
-			(day <= 10) {
-		alert('Первая декада');
-		}
-		if 
-			(day >= 11 && day <= 20) {
-		alert('Вторая декада');
-		}
-		
-		if 
-			(day >= 21 && day <= 31){
-		alert('Третья декада');
-		}
-17
-var month  = 21;
-		if 
-			(month == 12 || month <= 2) {
-		alert('Зима');
-		}
-		if 
-			(month >= 3 && month <= 5) {
-		alert('Весна');
-		}
-		
-		if 
-			(day >= 6 && day <= 8){
-		alert('Лето');
-		}
-		
-		if 
-			(day >= 9 && day <= 11){
-		alert('Осень');
-		}
-18
-var str  = 'abcde';
-		if (str[0] == 'a'){
-			alert('да');
-		}
-		else{
-			alert('Нет');
-		}
-19
-var str = '12345';
-			if (str [0] == '1' || str[1] == '2' || str[2] == '3'){
-				alert('да');
-			}
-			else {
-				alert('нет');
-			}
-20
-var str = '123';
-	var sum = Number(str[0]) +  Number(str[1]) +  Number(str[2]);
-		alert(sum);		
-21 
-
-var str = '456456';
-			var sum1 = Number(str[0]) +  Number(str[1]) +  Number(str[2]);
-			var sum2 = Number(str[3]) +  Number(str[4]) +  Number(str[5]);
-				if (sum1 == sum2){
-					alert('Да');
-				}
-				else {
-					alert('Нет');
-				}
-			
-		alert(sum);
+for (var n = 1000, iter = 0; n > 50; n /= 2, iter++);
+			alert(n);
+			alert(iter);
