7d79ee64c6c48852ca34778cc38b7ea700cbc1b8
heartright <rodex.lani@gmail.com>
2018-03-27 16:16:01 +0300

    fix_27_03

--- lesson 7/lesson_7.js
+++ lesson 7/lesson_7.js
@@ -26,44 +26,35 @@
 
 7
 var array1 = [1, 2, 3, 4, 5];
-		alert(array1.splice(0, 3));
+		alert(array1.slice(0, 3));
 
 8
-		var str = 'я учу javascript!';
-		var arr = str.split(' ');
-		alert(arr);
+		var array1 = [1, 2, 3, 4, 5];
+		alert(array1.spice(-2));
 		
 9
-	var str = 'я учу javascript!';
-		var arr = str.split('');
-		alert(arr);
-	
+	var array1 = [1, 2, 3, 4, 5];
+		array1.splice(1, 2);
+		alert(array1);
 10
-		var data = '2025-12-31';
-		var array = data.split('-')
-		alert(array[2] + '.' + array[1] + '.' + array[0]);
+		var array1 = [1, 2, 3, 4, 5];
+		var res = array1.splice(1, 3);
+		alert(res);
 
 11
-var array = ['я', 'учу', 'javascript', '!'];
-		alert(array.join('+'));
+var array1 = [1, 2, 3, 4, 5];
+		array1.splice(3, 0,  'a', 'b', 'c');
+		alert(array1);
 			
 12
-var str = 'я учу javascript';
-		var str1 = str.slice(0, 1).toUpperCase() + str.slice(1);
-		alert(str1);
+var array1 = [1, 2, 3, 4, 5];
+		array1.splice(1, 0, 'a', 'b');
+		array1.splice(6, 0, 'c');
+		array1.splice(8, 0, 'e');
+		alert(array1);
 13 
-var arr = [12, 15, 20, 25, 59, 79];
-		var summ = 0; 
-			for (var i = 0; i < arr.length; i++){
-				summ += arr[i];
-				var middle = summ / arr.length;
-			}
-			alert(middle);
+var array1 = [3, 4, 1, 2, 7];
+		alert(array1.sort());
 14
-var str = 'var_test_text';
-		var array = str.split('_');
-		for (var i = 1; i < array.length; i++ ){ 
-			array[i] = array[i].slice(0, 1).toUpperCase() + array[i].slice(1);
-		}
-		var strrez = array.join('');
-		alert(strrez);
+var obj = {js:'test', jq: 'hello', css: 'world'};
+		alert(Object.keys(obj));
