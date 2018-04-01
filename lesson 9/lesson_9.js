c520a657d72d3f2b8d46e3d669b927cf9d8a7c33
Heartright <rodex.lani@gmail.com>
2018-04-01 15:49:36 +0300

    fix_01_04

---
+++ lesson 9/lesson_9.js
@@ -0,0 +1,66 @@
+1
+		function sqr(num){
+				return num * num;
+			}
+
+2
+		function summ(a, b){
+				return a + b;
+			}
+3
+		function res(a, b, c){
+				return (a - b) / c;
+			}
+4
+	function get(num){
+				var arr = ['понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота', 'воскресенье'];
+				return arr [num - 1];
+			}
+5
+	var string = 'index.html';
+		if (string.substr(-5) == '.html') {
+			alert('true')
+		}
+		else {
+			alert('false')
+		}
+		
+	
+6
+		var array1 = ['js', 'css', 'jq'];
+		alert(array1.pop());
+
+7
+var array1 = [1, 2, 3, 4, 5];
+		alert(array1.slice(0, 3));
+
+8
+		var array1 = [1, 2, 3, 4, 5];
+		alert(array1.spice(-2));
+		
+9
+	var array1 = [1, 2, 3, 4, 5];
+		array1.splice(1, 2);
+		alert(array1);
+10
+		var array1 = [1, 2, 3, 4, 5];
+		var res = array1.splice(1, 3);
+		alert(res);
+
+11
+var array1 = [1, 2, 3, 4, 5];
+		array1.splice(3, 0,  'a', 'b', 'c');
+		alert(array1);
+			
+12
+var array1 = [1, 2, 3, 4, 5];
+		array1.splice(1, 0, 'a', 'b');
+		array1.splice(6, 0, 'c');
+		array1.splice(8, 0, 'e');
+		alert(array1);
+13 
+var array1 = [3, 4, 1, 2, 7];
+		alert(array1.sort());
+14
+var obj = {js:'test', jq: 'hello', css: 'world'};
+		alert(Object.keys(obj));
