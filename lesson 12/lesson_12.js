5102843371a3ceb8b29a4835036f696c1d69fd5c
heartright <rodex.lani@gmail.com>
2018-04-09 10:22:28 +0300

    fix_09_04

--- lesson 12/lesson_12.js
+++ lesson 12/lesson_12.js
@@ -8,24 +8,36 @@
 		alert(array);
 
 2
-	var str = '';
-		for(var i = 1; i < 10; i++ ){
-			str += i;
-		}
-		 alert(str);
+	var array = [];
+			for (var i = 1; i < 10; i++) {
+				var str = '';
+			for(var j = 0; j < i; j++) {
+				str += i;
+				}
+				array.push(str);
+			}
+			alert(array);
 
 3
-	var str = '';
-		for(var i = 9; i > 0; i-- ){
-			str += i;
+	function arrayFill(val, length){
+			var array = [];
+				for(var i = 0; i < length; i++){
+				array.push(val);
+			}
+			return array;
 		}
-		 alert(str);
+		alert(arrayFill('x', 5));
 4
-	var str = '';
-		for(var i = 1; i < 10; i++ ){
-			str += i + '-';
-		}
-		 alert(str);
+	function arr(array){
+			var summ = 0;
+			for(var i = 0; i < array.length; i++){
+				summ += array[i];
+				while (summ > 10) {
+					return i + 1;
+					}
+				}
+			}	
+		alert(arr([1, 2, 3, 3, 1, 5, 4, 6 ]));
 5
 	var str = '';
 		for(var i = 1; i <= 20; i++){
