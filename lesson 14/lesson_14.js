cf2b27283540546d00cd183454156c6aeadbb810
heartright <rodex.lani@gmail.com>
2018-04-12 16:54:52 +0300

    fix_12_04

--- lesson 14/lesson_14.js
+++ lesson 14/lesson_14.js
@@ -1,30 +1,30 @@
-1 
-		function isNumberInRange(num){
-				if(num > 0 && num < 10){
-					return true; 
-					}
-				else {
-					return false;
-				}
-			}
+1 Дана строка. Сделайте заглавным первый символ каждого слова этой строки. Для этого сделайте вспомогательную функцию ucfirst, которая будет получать строку, делать первый символ этой строки заглавным и возвращать обратно строку с заглавной первой буквой
+		function ucfirst(str){
+			return str[0].toUpperCase() + str.substr(1);
+		}
+		
+		var str = 'var text hello'
+		var array = str.split(' ');
+		for(var i = 0; i < array.length; i++){
+			array[i] = ucfirst(array[i]);
+		}
+		
+		str = array.join(' ');
+		alert(str);
 
-2
-	function isNumberInRange(num){
-				if(num > 0 && num < 10){
-					return true; 
-					}
-				else {
-					return false;
-				}
-			}
-			var array = [1, 2, 3, 11, 5, 13, 12];
-			var res = [];
-			for (var i = 0; i < array.length; i++){
-				if (isNumberInRange(array[i])){
-					res.push(array[i]);
-				}
-			}
-			alert(res)
+2 Дана строка вида 'var_text_hello'. Сделайте из него текст 'varTextHello'.
+	function ucfirst(str){
+			return str[0].toUpperCase() + str.substr(1);
+		}
+		
+		var str = 'var_text_hello'
+		var array = str.split('_');
+		for(var i = 1; i < array.length; i++){
+			array[i] = ucfirst(array[i]);
+		}
+		
+		str = array.join('');
+		alert(str);
 
 3
 	function getDigitsSum(digit){
